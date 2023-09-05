65984c35f405afe1f29938d07691284719dce0f7
Andrei Fridman <judoostudy@mail.ru>
2023-09-05 12:18:57 +0300

    second hw added

--- DrySolidPrinciplesHW4.2/DrySolidPrinciplesHW4.2.cpp
+++ DrySolidPrinciplesHW4.2/DrySolidPrinciplesHW4.2.cpp
@@ -3,7 +3,7 @@
 
 
 
-
+/*
 class Printable
 {
 public:
@@ -81,4 +81,72 @@ int main()
     saveTo(file, ctext);
     saveTo(file, cjson);
     
+}
+*/
+
+class Printable
+{
+public:
+    enum class Format
+    {
+        kText,
+        kHTML,
+        kJSON
+    };
+
+    virtual ~Printable() = default;
+    virtual std::string print() const = 0;
+};
+
+class HtmlData : public Printable {
+public:
+    HtmlData(std::string const& data) : data_(data) {}
+
+    std::string print() const override {
+        return "<html>" + data_ + "<html/>";
+    }
+private:
+    std::string data_;
+    Format format = Format::kHTML;
+
+};
+
+class TextData : public Printable {
+public:
+    TextData(std::string const& data) : data_(data) {}
+
+    std::string print() const override {
+        return data_;
+    }
+private:
+    std::string data_;
+    Format format_ = Format::kText;
+};
+
+class JsonData : public Printable {
+public:
+    JsonData(std::string const& data) : data_(data) {}
+
+    std::string print() const override {
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+private:
+    std::string data_;
+    Format format = Format::kJSON;
+};
+
+void saveTo(std::ofstream& file, const Printable& printable)
+{
+    file << printable.print() << std::endl;
+}
+
+int main()
+{
+    std::ofstream file("save_file.txt");
+    HtmlData html("This is html string");
+    TextData text("This is text string");
+    JsonData json("This is json string");
+    saveTo(file, html);
+    saveTo(file, text);
+    saveTo(file, json);
 }
